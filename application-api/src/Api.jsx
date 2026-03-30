d274e5e2fd77a2a08069270a6f76d71ac15274a8
Kephren-B <kephren.bib@ikmail.com>
2026-03-30 11:17:45 +0200

    api

---
+++ application-api/src/Api.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function Users() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, []);
+
+    return (
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>{user.name}</li>
+            ))}
+        </ul>
+    );
+}
