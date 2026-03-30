e807b81c0331a42f6b8324f3c06555805798081f
Kephren-B <kephren.bib@ikmail.com>
2026-03-30 11:53:34 +0200

    fonction de recherche

--- application-api/src/Home.jsx
+++ application-api/src/Home.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import "./Users.css";
+import UserSearch from "./Search";
 
 function Home({ users, loading }) {
+    const [query, setQuery] = useState("");
+
     if (loading) {
         return <div className="loading">Chargement des utilisateurs...</div>;
     }
 
+    const filteredUsers = users.filter(user =>
+        user.username.toLowerCase().includes(query.toLowerCase())
+    );
+
     return (
         <div className="users-container">
             <h1>Liste des Utilisateurs</h1>
+
+            {/* On passe query et setQuery à UserSearch */}
+            <UserSearch query={query} setQuery={setQuery} />
+
             <table className="users-table">
                 <thead>
                     <tr>
@@ -21,7 +33,7 @@ function Home({ users, loading }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {users.map(user => (
+                    {filteredUsers.map(user => (
                         <tr key={user.id}>
                             <td>{user.name}</td>
                             <td>@{user.username}</td>
